8b924b0fb6fd7564ccbf8927e9fb0f5a92d341e4
Moritz Herb <2023659@stud.hs-mannheim.de>
2025-10-30 13:53:59 +0100

    ersten 3 Kritische Situationen hinzugefügt

--- Remote-Monitoring von Vitaldaten/VitalDatenSimulator/MainWindow.xaml.cs
+++ Remote-Monitoring von Vitaldaten/VitalDatenSimulator/MainWindow.xaml.cs
@@ -36,6 +36,16 @@ namespace VitalDatenSimulator
         private double _updateIntervall = 1000;
         private bool isRunning = false;
 
+        private double minHR = 40, maxHR = 160;
+        private double minTemp = 34, maxTemp = 42;
+        private double minBP = 70, maxBP = 240;
+        private double minRR = 8, maxRR = 30;
+        private double minSpO2 = 80, maxSpO2 = 99; 
+
+        private bool simulateTachy = false;
+        private bool simulateHypoxia = false;
+        private bool simulateFever = false;
+
         public string SimulationButtonText => isRunning ? "Stop Simulation" : "Start Simulation";
 
         public double HeartRate { get => _heartRate; set { _heartRate = value; OnPropertyChanged(nameof(HeartRate)); } }
@@ -69,11 +79,23 @@ namespace VitalDatenSimulator
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            HeartRate = ChangeValue(HeartRate, 40, 160);
-            Temperature = ChangeValue(Temperature, 34, 42);
-            BloodPressure = ChangeValue(BloodPressure, 70, 240);
-            RespRate = ChangeValue(RespRate, 8, 30);
-            SpO2 = ChangeValue(SpO2, 80, 99);
+            if (simulateTachy)
+                HeartRate = SimulateCriticalChange(HeartRate, minHR, maxHR, +5);
+            else
+                HeartRate = ChangeValue(HeartRate, minHR, maxHR);
+
+            if (simulateFever)
+                Temperature = SimulateCriticalChange(Temperature, minTemp, maxTemp, +0.5);
+            else
+                Temperature = ChangeValue(Temperature, minTemp, maxTemp);
+
+            BloodPressure = ChangeValue(BloodPressure, minBP, maxBP);
+            RespRate = ChangeValue(RespRate, minRR, maxRR);
+
+            if (simulateHypoxia)
+                SpO2 = SimulateCriticalChange(SpO2, minSpO2, maxSpO2, -3);
+            else
+                SpO2 = ChangeValue(SpO2, minSpO2, maxSpO2);
 
             if (graphWindow != null && graphWindow.IsVisible)
             {
@@ -97,14 +119,16 @@ namespace VitalDatenSimulator
                 value = max;
 
             return value;
+        }
 
-           // double factor = 1 + ((rnd.NextDouble() * 2 - 1) * (ChangePercent / 100.0));
-            //double newValue = value * factor;
-
-           // if (newValue < min) { newValue = min; }
-            //if (newValue > max) { newValue = max; }
-          
-            //return newValue;
+        private double SimulateCriticalChange(double value, double min, double max, double step)
+        {
+            value += step;
+            if (value < min)
+                value = min;
+            if (value > max)
+                value = max;
+            return value;
         }
 
         private void ToggleSimulation(object sender, RoutedEventArgs e)
@@ -113,6 +137,9 @@ namespace VitalDatenSimulator
             {
                 timer.Stop();
                 isRunning = false;
+                simulateFever = false;
+                simulateHypoxia = false;
+                simulateTachy = false;
             }
             else
             {
@@ -147,6 +174,49 @@ namespace VitalDatenSimulator
             {
                 graphWindow.Focus();
             }
+        }
+
+        private void SimulateTachycardia(object sender , RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled) 
+                return;
+
+            if (!simulateTachy)
+                simulateTachy = true;
+            else
+                simulateTachy = false;
+            simulateFever = false;
+            simulateHypoxia = false;
+
+        }
+
+        private void SimulateFever(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+                return;
+            simulateTachy = false;
+
+            if (!simulateFever)
+                simulateFever = true;
+            else
+                simulateFever = false;
+
+            simulateHypoxia = false;
+
+        }
+
+        private void SimulateHypoxia(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+                return;
+            simulateTachy = false;
+            simulateFever = false;
+
+            if(!simulateHypoxia)
+                simulateHypoxia = true;
+            else
+                simulateHypoxia= false;
+
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
