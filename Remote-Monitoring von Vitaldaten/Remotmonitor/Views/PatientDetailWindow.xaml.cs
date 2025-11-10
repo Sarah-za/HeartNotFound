d9323baac3ffd506dfbc4904e10bcd339329e1e4
anas karim <anas.karim@stud.hs-mannheim.de>
2025-11-10 20:42:13 +0100

    Update: Patienten-Grid skalieren (4 → 16 Patienten), Patientenübersicht nach Zimmer, Übersicht verbessern, Farben angepasst

---
+++ Remote-Monitoring von Vitaldaten/Remotmonitor/Views/PatientDetailWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Remotmonitor
+{
+    public partial class PatientDetailWindow : Window
+    {
+        public PatientDetailWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
