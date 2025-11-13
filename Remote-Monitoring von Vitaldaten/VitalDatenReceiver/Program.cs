af03fa8cfc3c886fed8f4cdaeb3535875ff92888
Moritz Herb <2023659@stud.hs-mannheim.de>
2025-11-13 13:29:40 +0100

    Mqtt Receiver version1 added

---
+++ Remote-Monitoring von Vitaldaten/VitalDatenReceiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using VitalDatenReceiver;
+
+class Programm
+{
+    static async Task Main(string[] args)
+    {
+        VitalMqttSubscriber vr = new VitalMqttSubscriber();
+        await vr.Connect();
+
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("MQTT Vitaldaten Receiver gestartet");
+        Console.WriteLine("Drücke [Q], um das Programm zu beenden.\n");
+
+        while(true)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.Q)
+                    break;
+            }
+            await Task.Delay(100);
+        }
+
+        await vr.disconnect();
+        vr.printSummary();
+
+        Console.WriteLine("Drücke eine beliebige Taste zum schließen...");
+        Console.ReadKey();
+    }
+}
---
+++ Remote-Monitoring von Vitaldaten/VitalDatenReceiver/VitalMqttSubscriber.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+
+
+namespace VitalDatenReceiver
+{
+    internal class VitalMqttSubscriber
+    {
+        private readonly string broker = "mqtt.inftech.hs-mannheim.de";
+        private readonly int port = 8883;
+        private string clientId = Guid.NewGuid().ToString();
+        private readonly string username = "25pms02";
+        private readonly string password = "cf0fc303";
+
+        private IMqttClient myClient;
+        private MqttFactory myFactory;
+
+        private string stationId = "";
+        private List<(DateTime Time, string station, string parameter, string value)> dataList = new List<(DateTime,string, string, string)>();
+
+        public VitalMqttSubscriber()
+        {
+            myFactory = new MqttFactory();
+            myClient = myFactory.CreateMqttClient();
+        }
+
+        public async Task Connect()
+        {
+            var options = new MqttClientOptionsBuilder()
+                    .WithTcpServer(broker, port)
+                    .WithTlsOptions(
+                        o => o.WithCertificateValidationHandler(_ => true))
+                    .WithCredentials(username, password)
+                    .Build();
+
+            var connetctResult = myClient.ConnectAsync(options).GetAwaiter().GetResult();
+
+            if (connetctResult.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                Console.WriteLine("Erfolgreich verbunden mit: " + broker);
+                myClient.ApplicationMessageReceivedAsync += receivedMessage;
+
+                await myClient.SubscribeAsync("25pms02/+/heartrate");
+                await myClient.SubscribeAsync("25pms02/+/temperature");
+                await myClient.SubscribeAsync("25pms02/+/bloodpressure");
+                await myClient.SubscribeAsync("25pms02/+/resprate");
+                await myClient.SubscribeAsync("25pms02/+/sp02");
+
+                Console.WriteLine("Abonniert: 25pms02/<StationID>/(alleVitalparameter)\n");
+            }
+            else
+            {
+                Console.WriteLine($"Verbindung fehlgeschlagen: {connetctResult.ResultCode}");
+            }
+        }
+
+        private async Task receivedMessage(MqttApplicationMessageReceivedEventArgs e)
+        {
+            string topic = e.ApplicationMessage.Topic;
+            string payload = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment);
+
+            var parts = topic.Split('/');
+            if (parts.Length >= 3)
+            {
+                stationId = parts[1];
+                string station = parts[1];
+                string parameter = parts[2];
+                dataList.Add((DateTime.Now, station, parameter, payload));
+                Console.WriteLine($"[{DateTime.Now:T}] Station {station, -6} {parameter,-12}: {payload}");
+            }
+
+            await Task.CompletedTask;
+
+        }
+
+        public async Task disconnect()
+        {
+            var disconnectoptions = new MqttClientDisconnectOptionsBuilder()
+                                    .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
+                                    .Build();
+            await myClient.DisconnectAsync(disconnectoptions);
+            Console.WriteLine("Disconnected!");
+        }
+
+        public void printSummary()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Empfangene Vitaldaten");
+            Console.WriteLine("--------------------------------");
+
+            if (string.IsNullOrEmpty(stationId))
+            {
+                Console.WriteLine("Keine Station-ID erkannt.");
+                return;
+            }
+
+            Console.WriteLine("Station-ID: " + stationId + "\n");
+
+            if (dataList.Count == 0)
+            {
+                Console.WriteLine("Keine Daten empfangen.");
+                return;
+            }
+
+            foreach (var entry  in dataList)
+            {
+                Console.WriteLine($"{entry.Time:T} {entry.parameter,-12} {entry.value,6}");
+            }
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Gesamt: {dataList.Count} Datensätze gespeichert.\n");
+        }
+    }
+}
